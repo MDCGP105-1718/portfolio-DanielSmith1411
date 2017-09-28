762b41011cbdaa409b41a005ba259551b1c91966
Daniel Smith <dsmith3855195@gmail.com>
2017-09-28 12:09:38 +0100

    worked through the first 3 python examples

---
+++ ex1.py
@@ -0,0 +1,4 @@
+print ("Hello World!")
+print ("This is an excite")
+print ("Yeah, I get the idea")
+print ('Hmm, what would happen?') #Using single quotes instead of doubles has no effect other than looking less fancy.
---
+++ ex2.py
@@ -0,0 +1,10 @@
+print ("These are how many animal I has:")
+print ("Cow number", 15 + 30 / 6)
+print ("Sheep number", 100 - 30 * 3 % 4)#
+print ("Now, I will count the plant number:")
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print ("Is it true that 3 + 2 < 5 - 7?")
+print (3 + 2 < - 7)
+print ("What is 3 + 2?", 3 + 2)
+print ("What is 5 - 7?", 5 - 7)
+print ("Is it greater or equal?", 5 >= -2) #'>=' is kinda broke in Powershell. Comes up as a question mark.
