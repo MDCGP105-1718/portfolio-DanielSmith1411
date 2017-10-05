4b8ec9b9dad4cd0c89bbcd8583b6e402aa6da201
Daniel Smith <dsmith3855195@gmail.com>
2017-10-05 10:46:58 +0100

    [This is the 'bottles of beer' task. It should work eventually]

---
+++ ex5.py
@@ -0,0 +1,5 @@
+for n in range(99,0,-1): #Might replace this with a 'while' loop.
+        print(n, "cubes of cheese on the wall,", n, "cubes of cheese") #I prefer cheese
+        print("Take one down, pass it around,", n-1, "cubes of cheese on the wall") #There is probably a way to remove this second 'print' command.
+print ("No more cubes of cheese on the wall, no more cubes of cheese!") #I could add another couple of lines to remove the 's' from the end of 'cubes' when n or n-1 is equal to 1.
+print ("Go to the store and buy some more, 99 cubes of cheese on the wall!")
