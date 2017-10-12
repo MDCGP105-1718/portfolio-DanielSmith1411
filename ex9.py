b9bafa277f111090823da50a3a8829c9d917e08b
Daniel Smith <dsmith3855195@gmail.com>
2017-10-12 11:38:04 +0100

    UNFINISHED - saving here in case I can do any work on it at home

---
+++ ex9.py
@@ -0,0 +1,18 @@
+semi_annual_raise = 0.07
+annual_return_rate = 0.04
+r = 1.04
+down_payment = 0.25 * house_cost
+house_cost = 1000000.00
+months = 36
+epsilon = 100.00
+low = 0.00
+high = months
+guess = (high + low) / 2.00
+annual_salary = float(input("Enter your annual salary: "))
+portion_saved = float(input("Enter the percentage of your salary to save, as a decimal: "))
+monthly_salary = annual_salary / 12
+current_savings = current_savings + monthly_salary * portion_saved * r
+
+(months * monthly_salary * (0.07**(months/6)))
+
+while abs(guess)
