7083d48c28214fa947122a1732a84feb37d41703
danielsmith1411 <dsmith3855195@gmail.com>
2017-10-19 10:50:27 +0100

    Forgot to push this previously. Input task

---
+++ ex6.py
@@ -0,0 +1,20 @@
+my_name = input("Enter your name ")
+if my_name == "Daniel":
+    print("Hello again.") 
+else:
+    print("Hello,", my_name)
+my_age = int(input("Enter your age "))
+if my_age >=18:
+    print("You are probably very bored.")
+else:
+    print("Go and play Minecraft. You don't have much free time left to do it.")
+my_height = int(input("Enter your height in inches "))
+if my_height <=67:
+    print("Welcome to the short people club.")
+else:
+    print("Please duck down a bit. I can't see over you.")
+my_weight = int(input("Enter your weight in lbs "))
+if my_weight !=135:
+    print("Not going to make any comments about this, because I'm nice like that.")
+else:
+    print("Congratulations, you found the secret message!")
