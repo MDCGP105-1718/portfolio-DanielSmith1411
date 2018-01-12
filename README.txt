23c1fe20b0c91e3a035d59b88e02e1c349eb36ae
DanielSmith1411 <32357121+DanielSmith1411@users.noreply.github.com>
2018-01-12 11:27:00 +0000

    Add files via upload
    It doesn't work but I might as well submit something. Classes just don't work for me.

---
+++ README.txt
@@ -0,0 +1,25 @@
+1) Look Stairs
+2) Use Stairs
+3) Go Bedroom
+4) Look Drawer
+5) Take Crowbar
+6) Go ____
+7) Use Stairs
+8) Go Drawing Room
+9) Look Bookcase
+10) Take Book
+11) Inventory
+12) Book
+13) Back
+14) Go Hallway
+15) Go Kitchen
+16) Look Cellar
+17) Use Crowbar
+18) Go Cellar
+19) Look Chest
+20) Take Shiny Key
+21) Go Kitchen
+22) Go Hallway
+23) Look Door
+24) Use Key
+25) Go Outside
--- game.py
+++ game.py
@@ -1,21 +1,51 @@
-class Inventory(object):
-    def ___init__(self,items):
-        self.items = items
+looking='false'
 
-#class Map(object):
-#    def __init__(self,):
-#        self.
+class Hallway(object):
+    def __init__(self,locations):
+        self.locations=(Kitchen, DrawingRoom, Stairs)
+        self.locations=locations
+        self.looking=looking
+    def get_description(self):
+            print ('A LOOK around the hallway confirms that it has two ROOMS, one on the LEFT and the other on the RIGHT, and a set of STAIRS at the far end of it.')
+            looking='false'
+            inp='ready'
 
-#class Hallway(Map):
-#    def __init__(self,description):
-#        self.description = description
+    if looking=='true':
+        get_description
+    def kitchen(self):
+        return Kitchen()
 
-class Item(object):
+    #class Stairs(Hallway):
+    #    def __init__(sself):
+
+
+#    class Player(Hallway):
+#        def __init__(self,items):
+#            self.items = items
+
+class DrawingRoom(object):
+    def __init__(self,description):
+        self.description = description
+
+class Book(DrawingRoom):
+    def __init__(self,description):
+        self.description = description
+
+class Kitchen(object):
+    def __init__(self,description):
+        self.description = description
+
+class Cellar(object):
+    def __init__(self,description):
+        self.description = description
+
+class Key(Cellar):
     def __init__(self, description):
         self.description = description
         self.name = None
 
-#'Shiny Key' = Item('This key is shinier than the first. You recognise the colour from somewhere. Maybe retracing your steps will help you to find where it fits.')
+description = Key('This key is particularly shiny. You recognise the colour from somewhere. Maybe retracing your steps will help you to find where it fits.')
+
 
 #Toiletries = Item('You do not actually need these, but they smell nice.')
 
@@ -23,58 +53,83 @@ class Item(object):
 
 #class Toiletries(object):
 
-
-a=0
-
 def take(inpsplit):
   """
   Should print out the second item followed by a description of it.
   """
   #t=input('Choose item ')
-  print('Took the', inpsplit[1])
+  if len(inpsplit)==2 or (len(inpsplit)==3 and inpsplit[1]=='the'):
+      print('Took the', inpsplit[-1])
+      inp = 'ready'
+ # elif len(inpsplit)==3 and inpsplit[1]=='the':
+  else:
+      print('Try again')
+      inp = 'ready'
  # if inpsplit[1] == (''):
 
 def look(inpsplit):
     """
-    Prints the description of the item in inpsplit[1]
+    Prints the description of the thing in inpsplit[1]
     """
-    if inpsplit
-    print()
+    if len(inpsplit)==1:
+        looking='true'
+    else:
+        print ("error")
+        inp='ready'
 
-def open(inpsplit):
+def go(inpsplit):
     """
-    Opening doors
+    Should allow movement
     """
-    if inpsplit[1] == ("front door"):
-        if #player has key :
-            print ("")
-        else:
-            print ("")
-    elif inpsplit[1] == ("cellar door"):
-        if #player has key :
-            print ("")
-        else:
-            print ("")
+    if inpsplit[-1] in locations:
+        print('success')
+    else:
+        inp='ready'
+#def open(inpsplit):
+#    """
+#    Opening doors
+#    """
+#    if inpsplit[1] == ("front door"):
+#        if #player has key :
+#            print ("")
+#        else:
+#            print ("")
+#    elif inpsplit[1] == ("cellar door"):
+#        if #player has key :
+#            print ("")
+#        else:
+#            print ("")
 
-def commandprompt(a):
+def commandprompt(inp):
   """
   This is how the player interacts with the game.
   """
-  inp = input('What will you do?     ')
-  inpsplit = inp.split(' ')
-  #print(inpsplit[1])
-  if inpsplit[0] == ('take'):
-      take(inpsplit)
-  elif inpsplit[0] == ('open'):
-      open(inpsplit)
-  elif inpsplit[0] == ('look'):
-      look(inpsplit)
-  #elif inpsplit[0] == ('drop'):
-  else:
-      print('')
+  inp = 'ready'
+  while inp == 'ready':
+      inp = input('What will you do?     ')
+      inpsplit = inp.split(' ')
+      #print(inpsplit[1])
+      if inpsplit[0] == ('take'):
+          take(inpsplit)
+          inp = 'ready'
+      elif inpsplit[0] == ('go'):
+          go(inpsplit)
+          inp = 'ready'
+      elif inpsplit[0] == ('look'):
+          look(inpsplit)
+          inp = 'ready'
+      elif inpsplit[0] == ('end'):
+          print('Ended.')
+      else:
+          print('Wrong input')
+          inp = 'ready'
+
 
 print('You have found yourself in the hallway of a large building, possibly a mansion, by the general look of the place.')
 print('As you try to remember how you ended up here, you hear a creaking behind you.')
 print('You turn around to see the FRONT DOOR swing shut, making a loud "clunk".')
 print("A  LOOK  around the hallway confirms that it has two ROOMs, one on the  LEFT  and the other on the  RIGHT,  and a set of  STAIRS  at the far end of it.")
-commandprompt(a)
+#looking='false'
+inp='ready'
+location=Hallway
+commandprompt(inp)
